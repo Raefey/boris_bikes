dc75cf3d93ae8750bde27cf6f92707f8c2f281a7
Raefe <raefe.nj@gmail.com>
2018-02-20 14:44:40 +0000

    step7 back to the feature

---
+++ lib/docking_station.rb
@@ -0,0 +1,5 @@
+class DockingStation
+
+end
+docking_station = DockingStation.new
+docking_station.release_bike
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,3 +1,5 @@
+require './lib/docking_station.rb'
+DockingStation = Class.new
 describe DockingStation do
-  it "Releases a bike"
+  #it "Releases a bike"
 end
