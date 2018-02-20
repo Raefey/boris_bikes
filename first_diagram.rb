18f6ff179bfd48c56425758babf5af0c31f7803e
Alex <alejandropitarcholivas@gmail.om>
2018-02-20 12:09:47 +0000

    Add 2 working with user stories

---
+++ first_diagram.rb
@@ -0,0 +1,9 @@
+objects1 = ["person", "bike", "docking station"]
+messages1 = ["release_a_bike"]
+puts objects1
+puts messages1
+
+objects2 = ["person", "bike"]
+messages2 = ["bike_is_working"]
+puts objects2
+puts messages2
